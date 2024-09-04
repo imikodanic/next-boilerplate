8bc42fb0f33a26a7b6893c5862d6bb2670e47261
imikodanic <imikodanic@gmail.com>
2024-09-04 17:10:17 +0200

    fix: add font family to headers

--- theme/text-styles.ts
+++ theme/text-styles.ts
@@ -4,21 +4,25 @@ export const textStyles = defineTextStyles({
     h1: {
         value: {
             fontSize: "2.875rem",
+            fontFamily: "poppins",
         },
     },
     h2: {
         value: {
             fontSize: { base: "2.125rem", lg: "2.125rem" },
+            fontFamily: "poppins",
         },
     },
     h3: {
         value: {
             fontSize: { base: "1.875rem", lg: "1.875rem" },
+            fontFamily: "poppins",
         },
     },
     h4: {
         value: {
             fontSize: "1.625rem",
+            fontFamily: "poppins",
         },
     },
 });
